f56bbbcf1b18544b8aa8f80a4de1f0d1e07575ab
noaheakin <noaheakin@gmail.com>
2021-01-22 09:17:30 -0800

    commit tree 1210fc6835914a5a6201004cc7119a20d7df7026

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(arr) {
+    const results = []
+    for (let i = 0; i < arr.length; i++) {
+        results.push(arr[i] * -1)
+    }
+    return results
+}
+
+function mapToNoChange(arr) {
+    const results = []
+    for (let i = 0; i < arr.length; i++) {
+        results.push(arr[i])
+    }
+    return results
+}
+
+function mapToDouble(arr) {
+    const results = []
+    for (let i = 0; i < arr.length; i++) {
+        results.push(arr[i] * 2)
+    }
+    return results
+}
+
+function mapToSquare(arr) {
+    const results = []
+    for (let i = 0; i < arr.length; i++) {
+        results.push(arr[i] ** 2)
+    }
+    return results
+}
+
+function reduceToTotal(arr, start=0) {
+    let total = start
+    for (let i = 0; i < arr.length; i ++) {
+        total += arr[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) {
+            return true
+        }
+    }
+    return false
+}
